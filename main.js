43be6d91ab345c0bd32e2a877718559ceab48499
Giuseppe <festagiuseppe89@gmail.com>
2021-02-23 11:16:21 +0100

    Aggiunta commenti in js

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 
-
+//salviamo in una variabile il nostro BUTTON 'GENERA'
 var buttonGenera = document.getElementById('genera');
+
+//rimaniamo in ascolto di un evento click su quel button 'genera'
 buttonGenera.addEventListener( 'click',
 function() {
 
-    var inputNome = document.getElementById("nome-utente");
-    var nomeUtente = inputNome.value;
+    var inputNome = document.getElementById("nome-utente").value;
+    //var nomeUtente = inputNome.value;
 
-    var inputEta = document.getElementById("fascia-eta");
-    var eta = inputEta.value;
+    var inputEta = document.getElementById("fascia-eta").value;
+    //var eta = inputEta.value;
 
     var inputKm = document.getElementById("km");
     var percorrenza = parseInt(inputKm.value);
@@ -18,23 +20,23 @@ function() {
     var offerta = "Tariffa Standard";
 
 
-    if (eta == "minorenne") {
-        ((prezzoBiglietto / 100) * 80);
+    if (inputEta == "minorenne") {
+        prezzoBiglietto -= (prezzoBiglietto * 20 / 100); //prezzoBiglietto = prezzoBiglietto - (prezzoBiglietto * 20 / 100) or'prezzoBiglietto -= ()'
         offerta = "Sconto Minorenne";
-    } else if (eta == "over65") {
-        ((prezzoBiglietto / 100) * 60);
+    } else if (inputEta == "over65") {
+        prezzoBiglietto -= (prezzoBiglietto * 40 / 100);
         offerta = "Sconto Maggiorenne";
-    } else {
-        (prezzoBiglietto);
+    
     } 
     
-    var carrozza = Math.floor(Math.random() * 9) +1;
-    var cp = Math.floor(Math.random() * (100000 - 90000 + 1 )) + 90000;
+    //GENERIAMO UN NUMERO RANDOM PER CARROZZA E CP
+    var carrozza = Math.floor(Math.random() * 9) +1; //num compreso tra 1 e 9
+    var cp = Math.floor(Math.random() * (100000 - 90000 + 1 )) + 90000; //num compreso tra 90000 a 100000 escluso
     
-    document.getElementById('nome-passeggero').innerHTML = nomeUtente;
+    //stampiamo
+    document.getElementById('nome-passeggero').innerHTML = inputNome;
     document.getElementById('offerta').innerHTML = offerta;
-    document.getElementById('costo-biglietto').innerHTML = prezzoBiglietto.toFixed(2) + " Euro"
-    // toFixed(x) ARROTONDA A UN NUMERO DI DECIMALI SPECIFICATO
+    document.getElementById('costo-biglietto').innerHTML = prezzoBiglietto.toFixed(2) + " Euro" //"toFixed" lasciamo solo 2 decimali
     document.getElementById('carrozza').innerHTML = carrozza;
     document.getElementById('codice-cp').innerHTML = cp;
 
@@ -43,24 +45,26 @@ function() {
     bigliettoStampato.classList.add('show');
 });
 
+//salviamo in una variabile il nostro BUTTON 'ANNULLA'
 var buttonAnnulla = document.getElementById('annulla');
+
+//rimaniamo in ascolto di un evento click su quel button 'annulla'
 buttonAnnulla.addEventListener( 'click',
 function() {
+
+    //svuotiamo elementi
     document.getElementById('nome-passeggero').innerHTML = '';
     document.getElementById('offerta').innerHTML = '';
     document.getElementById('costo-biglietto').innerHTML = '';
     document.getElementById('carrozza').innerHTML = '';
     document.getElementById('codice-cp').innerHTML = '';
 
-
+    //svuotiamo gli input
     document.getElementById('km').value = '';
     document.getElementById("fascia-eta").value = '';
     document.getElementById("nome-utente").value = '';
 
-    document.getElementById('km').value = '';
-    document.getElementById("fascia-eta").value = '';
-    document.getElementById("nome-utente").value = '';
-    
+ 
     var bigliettoStampato = document.getElementById('biglietto')
     bigliettoStampato.classList.remove('show');
     bigliettoStampato.classList.add('hidden');
